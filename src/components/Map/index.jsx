a33472a8a3e7fc3df57b189e020c7c2104a8176a
Evgeny Karasev <gnomskg@gmail.com>
2020-09-23 15:27:18 +0300

    feat: load map config from server

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import { Map as LeafletMap, ImageOverlay, ZoomControl } from 'react-leaflet';
 
 import TablesEditingControl from '../TablesEditingControl';
@@ -11,13 +12,25 @@ import createMapConfiguration from './projection';
 
 import './index.css';
 
-const { bounds: mapBounds, crs } = createMapConfiguration(1, [1600, 1600], [0, 0], 0.05);
-
-const Map = () => {
+const Map = ({ config: { width, height, scale } }) => {
   const center = [0, 0];
+
+  const { bounds: mapBounds, crs } = useMemo(
+    () => createMapConfiguration(1, [width, height], center, scale),
+    [center, width, height, scale]
+  );
+
   return (
-    <LeafletMap id="root-map" center={center} zoom={0} crs={crs} zoomControl={false} useFlyTo>
-      <ImageOverlay url="http://95.181.230.223:15032/robot.jpg" bounds={mapBounds} />
+    <LeafletMap
+      id="root-map"
+      center={center}
+      maxBounds={mapBounds}
+      zoom={0}
+      crs={crs}
+      zoomControl={false}
+      useFlyTo
+    >
+      <ImageOverlay url={`http://${window.location.hostname}:15032/mapImage`} bounds={mapBounds} />
       <ZoomControl position="bottomright" />
       <TablesEditingControl position="topright" />
       <RobotsInformation position="topleft" />
@@ -26,6 +39,14 @@ const Map = () => {
       <LayerRobots />
     </LeafletMap>
   );
+};
+
+Map.propTypes = {
+  config: PropTypes.shape({
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    scale: PropTypes.number.isRequired
+  }).isRequired
 };
 
 export default Map;
